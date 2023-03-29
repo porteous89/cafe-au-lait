fe1dcec1b562fb31c1fd6439bb4587356e06c65f
Alister Porteous <alisterporteous@hotmail.com>
2023-03-29 13:17:59 -0400

    added cart, checkout and stripe components

--- server/routes/api/payments.js
+++ server/routes/api/payments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || require('../config/stripe').STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || require('../../config/stripe').STRIPE_SECRET_KEY);
 
 router.post('/charge', async (req, res) => {
   const { amount, token } = req.body;
