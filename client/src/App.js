40ae9572a109c4f5b2f8c5e3019b2db2a8804385
Nav <navdeep_aulakh24@hotmail.com>
2023-03-29 12:45:19 -0400

    worked in comp folder for styling, added pages folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import "./assets/css/styles.css";
+import Header from './components/Header';
+import Menu from './components/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Menu name="Cold Drinks"/>
+      <Menu name="Hot Drinks"/>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // Stripe API function
 // function App() {
