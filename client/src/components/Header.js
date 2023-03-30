79f3344981c1f480e445de36f38b02a476a00b81
Nav <navdeep_aulakh24@hotmail.com>
2023-03-30 13:45:00 -0400

    working in css files for menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({name, cart}) => {
     return (
         <nav className="navbar navbar-light navbar-expand-md sticky-top nav-bar">
             <div className="container">
-                <a className="navbar-brand" href="#">{name}</a>
+                <a className="navbar-brand" href="/">{name}</a>
                 <button data-bs-toggle="collapse" data-bs-target="#navcol-1" className="navbar-toggler">
                     <span className="visually-hidden">Toggle navigation</span>
                     <span className="navbar-toggle-icon"></span>
@@ -13,19 +13,22 @@ const Header = ({name, cart}) => {
                 <div className="collapse navbar-collapse" id="navcol-1">
                     <ul className="navbar-nav ns-auto">
                         <li className="nav-item">
-                            <a className="nav-link active" href="#">Menu</a>
+                            <a className="nav-link" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Profile</a>
+                            <a className="nav-link active" href="/">Menu</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Cart</a>
+                            <a className="nav-link" href="/">Profile</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Login</a>
+                            <a className="nav-link" href="/">Cart</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Tables</a>
+                            <a className="nav-link" href="/">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/Tables">Tables</a>
                         </li>
                         {/* <li className="nav-item">
                         <button onClick={} className="btn btn-outline-dark d-flex align-items-end" type="button">
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
 import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { UPDATE_ITEMS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
+import { QUERY_ITEMS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
@@ -12,50 +12,50 @@ function ProductList() {
 
   const { currentCategory } = state;
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_ITEMS);
 
   useEffect(() => {
     if (data) {
       dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
+        type: UPDATE_ITEMS,
+        items: data.items,
       });
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+      data.items.forEach((items) => {
+        idbPromise('items', 'put', items);
       });
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
+      idbPromise('items', 'get').then((items) => {
         dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
+          type: UPDATE_ITEMS,
+          items: items,
         });
       });
     }
   }, [data, loading, dispatch]);
 
-  function filterProducts() {
+  function filterItems() {
     if (!currentCategory) {
-      return state.products;
+      return state.items;
     }
 
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
+    return state.items.filter(
+      (item) => item.category._id === currentCategory
     );
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
+      {state.items.length ? (
         <div className="flex-row">
-          {filterProducts().map((product) => (
+          {filterItems().map((item) => (
             <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
+              key={item._id}
+              _id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+              quantity={item.quantity}
             />
           ))}
         </div>
