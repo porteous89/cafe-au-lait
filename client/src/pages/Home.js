f94c6ec3aa1c31a70269016279c47fa70c27ac2a
Alister Porteous <alisterporteous@hotmail.com>
2023-04-03 10:05:38 -0400

    changed title to au from du

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,7 +20,7 @@ const Home = ({ name }) => {
     <Container fluid className="home-container">
       <div className="home">
         <Card className="welcome-card">
-          <h1>Welcome to Cafe Du Lait</h1>
+          <h1>Welcome to Cafe Au Lait</h1>
           <p>A small French cafe off the water, in the heart of downtown.</p>
           <p>
             Enjoy a cup of coffee and a pastry while you take in the view and
